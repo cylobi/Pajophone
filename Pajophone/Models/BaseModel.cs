ce545833bc86625658649c041d62cb110992bc84
cylobi <javad.afsari01@gmail.com>
2024-05-19 19:16:06 +0330

    feat : added ProductModel

---
+++ Pajophone/Models/BaseModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Pajophone.Models;
+
+public class BaseModel
+{
+    [DataType(DataType.Date)]
+    public DateTime CreatedAt { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime LastEditedAt { get; set; }
+}
---
+++ Pajophone/Models/ProductModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+
+namespace Pajophone.Models;
+
+public class ProductModel : BaseModel
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Color { get; set; }
+    public string ImageUrl { get; set; }
+}
