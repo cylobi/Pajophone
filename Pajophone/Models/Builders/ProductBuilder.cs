d2053c2a4de7e56ac8fe39ee2fb73f0f0e6f0fdc
cylobi <javad.afsari01@gmail.com>
2024-05-23 18:24:58 +0330

    feat: added basic product builder

---
+++ Pajophone/Models/Builders/ProductBuilder.cs
@@ -0,0 +1,60 @@
+﻿namespace Pajophone.Models.Builder;
+
+public class ProductBuilder
+{
+        protected ProductModel Product { get; set; }
+
+        public ProductBuilder()
+        {
+                Product = new ProductModel();
+        }
+
+        public ProductBuilder BuildBasicProduct(string name, string description, string color)
+        {
+                Product.Name = name;
+                Product.Description = description;
+                Product.Color = color;
+                return this;
+        }
+        
+        public ProductBuilder SetId(int id)
+        {
+                Product.Id = id;
+                return this;
+        }
+
+        public ProductBuilder AddImage(IFormFile file)
+        {
+                Product.Image = ConvertIFormFileToByteArray(file);
+                return this;
+        }
+        
+        public ProductBuilder AddCategory(string name)
+        {
+                ProductCategoryModel category = new ProductCategoryModel();
+                category.Name = name;
+                Product.Category = category;
+                return this;
+        }
+        
+        public ProductBuilder AddField(string key, string value)
+        {
+                ProductFieldModel field = new ProductFieldModel();
+                field.key = key;
+                field.value = value;
+                Product.ExtraFields.Add(field);
+                return this;
+        }
+
+        public ProductModel Build()
+        { 
+                return Product;
+        }
+        
+        protected byte[] ConvertIFormFileToByteArray(IFormFile formFile)
+        {
+                using var memoryStream = new MemoryStream();
+                formFile.CopyTo(memoryStream);
+                return memoryStream.ToArray();
+        }
+}
---
+++ Pajophone/Models/Builders/ProductBuilderFromView.cs
@@ -0,0 +1,13 @@
+﻿using Pajophone.ViewModels;
+
+namespace Pajophone.Models.Builder;
+
+public class ProductBuilderFromView : ProductBuilder
+{
+    public ProductBuilderFromView(ProductViewModel productModelView)
+        : base()
+    {
+        BuildBasicProduct(productModelView.Name, productModelView.Description, productModelView.Color);
+    }
+        
+}
