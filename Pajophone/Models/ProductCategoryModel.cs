22c44031d89c36d6e851d786d49354f05cad1699
cylobi <javad.afsari01@gmail.com>
2024-05-23 14:38:54 +0330

    feat: added ProductCategoryModel.cs

---
+++ Pajophone/Models/ProductCategoryModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Pajophone.Models;
+
+public class ProductCategoryModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = default!;
+    public int? ParentCategoryId { get; set; }
+    public ProductCategoryModel? ParentCategory { get; set; }
+}
